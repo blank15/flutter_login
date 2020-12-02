5f596ec5d91e06d9ae88ddc5e422e86ccee8b180
Yudistiro Septian Dwi Saputro <yudistiro6@gmail.com>
2020-12-02 23:45:44 +0700

    - add comming soon

--- lib/component/bodyWelcome.dart
+++ lib/component/bodyWelcome.dart
@@ -6,12 +6,23 @@ import 'package:flutter_svg/svg.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_example/component/background.dart';
 
-class BodyWelcome extends StatelessWidget {
+
+
+class BodyWelcome extends StatefulWidget {
+  @override
+  _BodyWelcomeState createState() => _BodyWelcomeState();
+}
+
+class _BodyWelcomeState extends State<BodyWelcome> {
+  bool showText = false;
+  double _opacityText = 0.0;
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery
         .of(context)
         .size;
+
     return Background(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -39,9 +50,25 @@ class BodyWelcome extends StatelessWidget {
                 child: RoundedButton(
                   text: Text("Sign Up"),
                   color: kPrimaryLightColor,
-                  pressed: (){},
-                ),)
+                  pressed: () {
+                    setState(() {
+                      if(showText){
+                        showText = !showText;
+                        _opacityText = 1.0;
+                      }else{
+                        showText = !showText;
+                        _opacityText = 0.0;
+                      }
+                    });
+                  },
+                ),),
+              Opacity(opacity:_opacityText,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Comming Soon",style: TextStyle(color: Colors.black,fontSize:14.0)),
+              ))
             ]
         ));
   }
 }
+
